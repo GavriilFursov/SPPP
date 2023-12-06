e2a527863e86debe294381502835e79c2eca5596
GavriilFursov <from.skveki1@mail.ru>
2023-12-06 18:11:18 +0300

    Initial commit

---
+++ SPPP/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SPPP
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.conectPort = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonConnect.Location = new System.Drawing.Point(10, 30);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(275, 95);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Подключиться";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // conectPort
+            // 
+            this.conectPort.BaudRate = 115200;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(884, 521);
+            this.Controls.Add(this.buttonConnect);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Система формирования отчетной документации СППП";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.IO.Ports.SerialPort conectPort;
+    }
+}
+
---
+++ SPPP/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Management;
+
+namespace SPPP
+{
+    public partial class MainForm : Form
+    {
+        bool isConnected = false; // Флаг проверки подключения
+
+        
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // Кнопка подключиться
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+             if(!isConnected) //Проверка флага, если false, подключаемся к плате управления, если true отключаемся
+            {
+                ConnectOrDisconnectToArduino(); // Функция подключения / отключения от платы управления
+            }
+            else
+            {
+                disconnectFromArduino(); // Функция отключения от платы управления
+            }
+        }
+
+        // Функция отключения от платы управления
+        private void disconnectFromArduino()
+        {
+            if(isConnected)
+            {
+                isConnected = false; // Меняем флаг
+                conectPort.Close(); // Закрываем порт
+                buttonConnect.Text = "Подключиться"; // Меняем текст кнопки
+            }
+        }
+
+        // Функция подключения / отключения от платы управления
+        private void ConnectOrDisconnectToArduino()
+        {
+            if(!isConnected) 
+            {
+                string arduinoPort = DetectArduinoPort(); // Записываем порт, к которому подключена плата упраления
+                if(!string.IsNullOrEmpty(arduinoPort)) // Если плата управления обнаружена, то подлючаемся
+                {
+                    isConnected = true; // Меняем флаг подключения
+                    conectPort.PortName = arduinoPort; // Присваем название порта
+                    conectPort.Open(); // Открываем порт
+                    buttonConnect.Text = "Отключиться"; // Меняем текст кнопки
+                }
+                // Если плата управления не обнаружена
+                else
+                {
+                    MessageBox.Show("Плата управления не обнаружена. Убедитесь, что она подключена.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            // Если флаг подключения true, отключаемся
+            else
+            {
+                disconnectFromArduino(); // Функция для отключения от платы управления
+            }
+        }
+
+        // Функция для обнаружения платы управления
+        private string DetectArduinoPort()
+        {
+            ManagementScope connectionScope = new ManagementScope();
+            SelectQuery serialQuery = new SelectQuery("SELECT * FROM Win32_SerialPort");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(connectionScope, serialQuery);
+            try
+            {
+                foreach(ManagementObject item in searcher.Get())
+                {
+                    string desc = item["Description"].ToString();
+                    string deviceId = item["DeviceID"].ToString();
+
+                    if (desc.Contains("Arduino"))
+                    {
+                        return deviceId;
+                    }
+                }
+            }
+            catch (ManagementException) 
+            {
+
+            }
+            return null;
+        }
+    }
+}
